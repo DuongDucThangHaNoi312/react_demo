62b315e0dbcb170c26a9ae2bcb78d28d880bf7e7
thang <thangdd@gmail.com>
2025-02-28 16:27:42 +0700

    add callback  lesson 22

--- JS cơ bản/bài21/main.js
+++ JS cơ bản/bài21/main.js
@@ -1,13 +1,13 @@
 import { sum } from "./sum.js";
-import { AA,BB,CC } from "./const.js";
+import { AA, BB, CC } from "./const.js";
 
 console.log('main.js');
 
 let a = 10;
 let b = 20;
 let c = sum(a, b);
-console.log(c);
 
+console.log(c);
 console.log('const AA', AA);
 console.log('const BB', BB);
 console.log('const CC', CC);
